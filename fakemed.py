1cb2b54a9ff9db58ef97332f439ef8a63a6130b9
Brandon <brandon.payne20@gmail.com>
2025-04-16 08:55:15 -0700

    Modified all f(x) names to have 'get_' before.
    get_dob now includes format as an optional argument and defaults to y-m-d if None.
    
    Added 'get_patient_profile' f(x) to obtain a name and dob. As new f(x)s are added, will amend these to get_patient_profile dict result.

--- fakemed.py
+++ fakemed.py
@@ -5,7 +5,7 @@ import datetime as date
 male_name_number = len(male_names)
 female_name_number = len(female_names)
 
-def first_name(sex=''):
+def get_first_name(sex=''):
     if sex == '':
         return rand.choice(male_names + female_names)
 
@@ -18,17 +18,19 @@ def first_name(sex=''):
         raise ValueError("Sex must be 'M', 'F', or ''")
 
     
-def last_name():
+def get_last_name():
     return rand.choice(last_names)
 
-def full_name(sex=''):
-    first = first_name(sex)
-    last = last_name()
+
+def get_full_name(sex=''):
+    first = get_first_name(sex)
+    last = get_last_name()
     return first + ' ' + last
 
-def date_of_birth():
+
+def get_dob(min_age=0, max_age=120, format=None):
     current_year = date.datetime.now().year
-    year = rand.randint(current_year - 100, current_year - 1)
+    year = rand.randint(current_year - max_age, current_year - min_age)
     month = rand.randint(1, 12)
     if month == 2:
         day = rand.randint(1, 28)
@@ -37,8 +39,26 @@ def date_of_birth():
     else:
         day = rand.randint(1, 31)
 
-    return f"{year}-{month:02d}-{day:02d}"
+    if format == None or format == 'y-m-d':
+        return f"{year}-{month:02d}-{day:02d}"
+    elif format == 'm/d/y':
+        return f"{month:02d}/{day:02d}/{year}"
+    elif format == 'y/m/d':
+        return f"{year}/{month:02d}/{day}"
+    elif format == 'd/m/y':
+        return f"{day:02d}/{month:02d}/{year}"
+    else:
+        raise ValueError("Invalid format. Use 'd/m/y', 'm/d/y', 'y/m/d', or 'y-m-d'.")
 
-print(full_name())
 
-print(date_of_birth())
+def get_patient_profile(sex='', min_age=0, max_age=120, format=None):
+    first = get_first_name(sex)
+    last = get_last_name()
+    dob = get_dob(min_age, max_age, format)
+    patient_data = {'first_name': first,
+                    'last_name': last,
+                    'dob': dob
+                    }
+    return patient_data
+
+print(get_patient_profile('F', 18, 25))
