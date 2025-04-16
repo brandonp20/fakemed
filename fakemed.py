821b9aa25e379e590cf2966f0845dfb2d8eaea5f
Brandon <brandon.payne20@gmail.com>
2025-04-16 07:47:27 -0700

    Added function for date_of_birth. Need to adjust to take at least age or approx age as a parameter.

--- fakemed.py
+++ fakemed.py
@@ -1,5 +1,6 @@
 import random as rand
 from names_data import male_names, female_names, last_names
+import datetime as date
 
 male_name_number = len(male_names)
 female_name_number = len(female_names)
@@ -25,4 +26,19 @@ def full_name(sex=''):
     last = last_name()
     return first + ' ' + last
 
+def date_of_birth():
+    current_year = date.datetime.now().year
+    year = rand.randint(current_year - 100, current_year - 1)
+    month = rand.randint(1, 12)
+    if month == 2:
+        day = rand.randint(1, 28)
+    elif month in [4, 6, 9, 11]:
+        day = rand.randint(1, 30)
+    else:
+        day = rand.randint(1, 31)
+
+    return f"{year}-{month:02d}-{day:02d}"
+
 print(full_name())
+
+print(date_of_birth())
